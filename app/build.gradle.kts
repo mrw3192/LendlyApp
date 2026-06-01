7b2717706aa7181df579800d3caddd6906733804
Marcelo Wainschenker <mwain@live.com>
2026-06-01 01:14:37 -0300

    Update CameraX, DataStore and Compose BOM to versions that support 16KB native library alignment natively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,4 +115,5 @@ dependencies {
   implementation(libs.androidx.camera.camera2)
   implementation(libs.androidx.camera.lifecycle)
   implementation(libs.androidx.camera.view)
+  implementation(libs.androidx.graphics.path)
 }
